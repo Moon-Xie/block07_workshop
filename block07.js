763b3157d85542291c1995d43aac38b9ba43cefd
Yue Xie <moonxie9304@gmail.com>
2024-11-15 01:19:16 +0700

    Add .

---
+++ block07.js
@@ -0,0 +1,22 @@
+/* 
+Pseudocode : How will I solve this excercise
+1.Open the block07 workshop : Secure the Vault on the Fullstack Canvas;
+2.make a block07 file on local;
+3.make a block07.js under the block07 file;
+4.code in block07.js
+5.use alert to create the string which will pupups on html with a dialog box for users;
+6.use let to assign 3 variables with value;
+7.use // to make comments for each code.
+*/
+
+//Create a string for the user, and use alert to display a popup dialog box
+alert('You have received this message because you have been chosen to open an important vault. Here is the secret combination:');
+
+//Declare a variable "sum1" and assign it
+let sum1 = 4 + 5 ;
+
+//Declare a variable "sum2" and assign it
+let sum2 = sum1 * 6 ;
+
+//Declare a variable "sum3" and assign it
+let sum3 = sum2 / 3;
